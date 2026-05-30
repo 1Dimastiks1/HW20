9e3e1c2a9cfc06b9d610956a62a8a160f7656c66
1Dimastiks1 <easports2031@gmail.com>
2026-05-30 22:58:18 +0300

    update js

--- index.js
+++ index.js
@@ -2,18 +2,6 @@ const images = document.querySelectorAll(".image");
 
 let currentIndex = 0;
 
-
-function showImage(index) {
-  images[index].scrollIntoView({
-    behavior: "smooth",
-    inline: "center",
-    block: "nearest"
-  });
-
-  images[index].focus();
-}
-
-
 document.addEventListener("keydown", (event) => {
   if (event.key === "ArrowRight") {
     currentIndex = (currentIndex + 1) % images.length;
